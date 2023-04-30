c8e6ee009f261ea2897f6d49e1759a34a6a2b415
unknown <cgiron681@gmail.com>
2023-04-30 20:14:31 +0800

    Make an user input

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,12 @@
-print("Hello World")
-print('My name is christoper')
+print('====================================================')
+print('===============User Input Excercises================')
+print('====================================================')
+name = input('Please enter your name: ')
+name = str(name)
+
+age = input('Please enter your age: ')
+age = int(age)
+
+print('My name is ' + name)
+print('And my age is ' + str(age))
+
